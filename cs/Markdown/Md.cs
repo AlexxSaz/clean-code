ee8842ea4c355b8875052f5995e64c53d0f5f96c
Aleksandr Sazonov <alexxsaz96@gmail.com>
2024-11-21 20:18:15 +0500

    Initial commit

---
+++ cs/Markdown/Md.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Markdown;
+
+public class Md
+{
+    public string Render(string text)
+    {
+        throw new NotImplementedException();
+    }
+}
