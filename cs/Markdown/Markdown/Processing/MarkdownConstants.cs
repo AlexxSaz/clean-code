fd6a1a06b2af6f6d088647175d2b249c8e663d48
Alexxsaz <alexxsaz96@gmail.com>
2024-11-27 17:33:22 +0500

    Added markdown token processor

---
+++ cs/Markdown/Markdown/Processing/MarkdownConstants.cs
@@ -0,0 +1,18 @@
+﻿namespace Markdown.Markdown.Processing;
+
+public static class MarkdownConstants
+{
+    public static class Symbols
+    {
+        public const string SingleEmphasis = "_";
+        public const string DoubleEmphasis = "__";
+        public const string Header = "#";
+    }
+
+    public static class ErrorMessages
+    {
+        public const string TokensNull = "Token collection cannot be null.";
+        public const string ContextNull = "Processing context cannot be null.";
+        public const string TokenNull = "Token cannot be null.";
+    }
+}
---
+++ cs/Markdown/Markdown/Processing/MarkdownProcessor.cs
@@ -0,0 +1,31 @@
+﻿using Markdown.Markdown.Handlers;
+using Markdown.Markdown.Tokens;
+
+namespace Markdown.Markdown.Processing;
+
+public class MarkdownProcessor
+{
+    private readonly IEnumerable<ITokenHandler> _tokenHandlers =
+        TokenHandlerFactory.CreateHandlers();
+
+    public IList<MarkdownToken> Process(IList<MarkdownToken> tokens)
+    {
+        var tokenContext = new TokenProcessingContext(tokens);
+        var resultContextModifier = new TokenProcessingContextModifier(tokenContext);
+
+        while (tokenContext.HasMoreTokens)
+        {
+            foreach (var handler in _tokenHandlers)
+            {
+                if (handler.CanHandle(tokenContext.CurrentToken) &&
+                    handler.Handle(tokenContext))
+                    break;
+
+                resultContextModifier.AddTokenToResult(tokenContext.CurrentToken);
+                tokenContext.CurrentIndex++;
+            }
+        }
+
+        return tokenContext.Result;
+    }
+}
